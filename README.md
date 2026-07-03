be6c291629b36b9d716fe6bf4b1fa00f51e608fa
pradip <pradip@zenithbyteinnovations.com>
2026-07-03 17:55:33 +0530

    requirements added

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Object-Detection
+
+## How to Run
+
+```bash
+conda create -n signlang python=3.7 -y
+```
+
+```bash
+conda activate signlang
+```
+
+```bash
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="signLanguage",
+    version="0.0.0",
+    author="Pradip",
+    packages=find_packages(),
+    install_requires=[]
+)
